538afdbdaf64869dcab9e7fada2a8474d6bdeed1
Rahul kumar <alexrahul9576@gmail.com>
2024-12-13 15:19:06 +0530

    Looping news from sampleoutput json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import React, {Component} from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News'
 
-
 export default class App extends Component {
     render(){
         return (
